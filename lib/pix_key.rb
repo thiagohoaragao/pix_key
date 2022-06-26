4b97cc1c1a7246329cf85be20a720ff6f41391e8
thiago aragao <thiagohoaragao@gmail.com>
2022-06-26 20:52:45 -0300

    added types

--- lib/pix_key.rb
+++ lib/pix_key.rb
@@ -8,44 +8,71 @@ class PixKey
   end
 
   def valid?
-    self.key.match?(/^[a-z0-9.]+@[a-z0-9]+.[a-z]+(.[a-z]+)?$/i)
+    validation = /(^[0-9]{11}$|^[0-9]{14}$|^\+[1-9][0-9]\d{1,14}$|^[a-z0-9.]+@[a-z0-9]+.[a-z]+(.[a-z]+)?$|[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12})/
+    @key.match?(validation)
   end
 
   def invalid?
-    self.key.class != String
+    if @key.class != String
+      true
+    elsif valid?
+      false
+    else
+      true
+    end
   end
 
   def value
-    '' if invalid?
+    if invalid?
+      ''
+    else
+      @key
+    end
   end
 
   def key
+    value
   end
 
   def to_s
+    @key.to_s
   end
 
   def phone?
+    @key.match?(/^\+[1-9][0-9]\d{1,14}$/i)
   end
 
   def cpf?
+    @key.match?(/^[0-9]{11}$/i)
   end
 
   def email?
+    @key.match?(/^[a-z0-9.]+@[a-z0-9]+.[a-z]+(.[a-z]+)?$/i)
   end
 
   def cnpj?
+    @key.match?(/^[0-9]{14}$/i)
   end
 
   def evp?
+    @key.match?(/[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}/i)
   end
 
   def type
+    if phone?
+      'phone'
+    elsif cpf?
+      'cpf'
+    elsif email?
+      'email'
+    elsif cnpj?
+      'cnpj'
+    else
+      'evp'
+    end
+  end
+
+  def ==(key)
+    @key.uniq
   end
 end
-
-#email regex
-#/^[a-z0-9.]+@[a-z0-9]+\.[a-z]+(\.[a-z]+)?$/i
-
-#cpf regex
-#/^[0-9]{11}$/i
